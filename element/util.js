55425a5ba5e152a1469b3297ed7522fcaf118676
David <davidpetrasek@hotmail.cz>
2024-04-14 08:48:24 +0200

    egseg

--- element/util.js
+++ element/util.js
@@ -165,7 +165,7 @@ export function htmlToElements(html)
 {
     var template = document.createElement('template');
     template.innerHTML = html;
-    return template.content;
+    return template.content.childNodes;
 }
 
 /**
@@ -179,7 +179,7 @@ export function elemInsert (elements, referenceNode, after = false)
 	
 	if (isString(elements)) 
 	{
-		elements = htmlToElements(elements).childNodes; 		//cLog('elements', elements, elemInsert);
+		elements = htmlToElements(elements); 		//cLog('elements', elements, elemInsert);
 		
 		elements = Array.from(elements);
 	}
