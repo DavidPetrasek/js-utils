0ed2767086630629dce3a3f4b279b248db470d63
David Petrásek <davidpetrasek@hotmail.cz>
2025-02-10 17:34:51 +0100

    gege

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import * as str from './string';
 import * as func from './function';
 import * as el from './element/util';
 import * as form from './element/form';
-import * as ajax from './ajax/util';
 import * as arr from './array';
 import * as file from './file';
 import * as is from './is';
 
-export default {misc, symfony_misc, str, func, el, ajax, arr, file, is, form};
+export default {misc, symfony_misc, str, func, el, arr, file, is, form};
